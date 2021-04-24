10d7f43b9d613f94057c10b1ff89128f52982f18
NAIBAFFF <fab.andreasson@gmail.com>
2021-04-25 00:39:09 +0200

    Add with agony
    This commit has been a learning experience that target can become painfully large and that git commits should be kept track off- especially with ggez!
    
    - Add basic code skeleton
    - Add TODO(s) for later
    - Add ggez import
    - Remove evil target folder
    - Add one (1) sleepless night and suffering

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # kevinwe-faband-nhopkins-project
 
-see [SPECIFICATIONS.md](https://github.com/INDAPlus20/kevinwe-faband-nhopkins-project/blob/main/SPECIFICATIONS.md)
+## Specifications
+
+For program specifications, see [SPECIFICATIONS.md](https://github.com/INDAPlus20/kevinwe-faband-nhopkins-project/blob/main/SPECIFICATIONS.md)
+
+## Architecture
+
+The architecture of the game is represented as follows:
+
+### Class structure
+
+![](./architecture/structure_0.1.0.PNG)
+
+### General Architecture
+
+![](./architecture/architecture_0.1.0.PNG)
--- src/src/main.rs
+++ src/src/main.rs
@@ -1,4 +1,39 @@
-fn main() {
-    // FIX: Graphics
-    //println!("Hello, world!");
+/**
+ * KThCCG Frontend.
+ * Author: Fabian Andréasson <fab.andreasson@gmail.com>
+ * Last updated: 2021-04-24
+ */
+// std
+use std::io;
+use std::path;
+
+// TODO: Imports from backend
+
+// ggez
+use ggez::event;
+use ggez::event::{EventHandler, KeyCode, KeyMods, MouseButton};
+use ggez::graphics::{self, Color, DrawMode, DrawParam};
+use ggez::GameResult;
+
+/// Size of the application window. TODO: Fix when art is made
+const SCREEN_SIZE: (f32, f32) = (420.0, 420.0);
+
+fn main() -> GameResult {
+    // Import path for graphical resources
+    // TODO: Make graphical resources
+    let resource_directory = path::PathBuf::from("./resources");
+
+    // Creates and configures ggez context
+    // TODO: Configure correctly
+    let context_builder = ggez::ContextBuilder::new("temp", "kevinwe-faband-nhopkins")
+        .add_resource_path(resource_directory)
+        .window_setup(ggez::conf::WindowSetup::default())
+        .window_mode(
+            ggez::conf::WindowMode::default()
+                .dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1)
+                .resizable(false),
+        );
+    // Builds context
+    let (context, event_loop) = &mut context_builder.build()?;
+    Ok(())
 }
