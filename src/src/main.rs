702945735ec67373a8e250171d0ac17e3a8331a4
NAIBAFFF <fab.andreasson@gmail.com>
2021-05-17 14:16:00 +0200

    Add basic skeleton for GUI - Open for further implementation when backend supports it - Comments for what needs to be done

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,26 +1,63 @@
 /**
  * KThCCG Frontend.
  * Author: Fabian Andréasson <fab.andreasson@gmail.com>
- * Last updated: 2021-04-24
+ * Last updated: 2021-05-17
  */
 // std
 use std::io;
 use std::path;
 
 // TODO: Imports from backend
+// This can be fixed in another branch
 
 // ggez
 use ggez::event;
 use ggez::event::{EventHandler, KeyCode, KeyMods, MouseButton};
 use ggez::graphics::{self, Color, DrawMode, DrawParam};
-use ggez::GameResult;
+use ggez::{Context, GameResult};
 
 /// Size of the application window. TODO: Fix when art is made
 const SCREEN_SIZE: (f32, f32) = (420.0, 420.0);
 
+/// GUI Structure and logic implementation
+struct AppState {
+    /// Image of the cards and relevant knowledge for
+    /// how it should handle inputs
+    sprites: Vec<graphics::Image>,
+    /// Implementation of backend board
+    // TODO: Implement it :P
+    board: i64,
+}
+
+/// Implementation of basic functions of the frontend program
+impl AppState {
+    fn new() -> GameResult<AppState> {
+        // TODO: Implement once backend exists
+    }
+    fn load_images() -> Vec<graphics::Image> {
+        // TODO: Implement graphics and proper loading function
+        vec![]
+    }
+}
+
+/// Implementeation of the eventloop in the frontend
+impl event::EventHandler for AppState {
+    /// Updating function
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+    /// For drawing interface and graphical representation
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
+        // TODO: Implement graphics and locations of things
+        Ok(())
+    }
+    // Implement functions for events of key presses, etc
+}
+
 fn main() -> GameResult {
     // Import path for graphical resources
     // TODO: Make graphical resources
+    // They do not need to be complex
     let resource_directory = path::PathBuf::from("./resources");
 
     // Creates and configures ggez context
@@ -36,4 +73,6 @@ fn main() -> GameResult {
     // Builds context
     let (context, event_loop) = &mut context_builder.build()?;
     Ok(())
+
+    // TODO: Input handler
 }
