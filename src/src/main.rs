962270ef693e11a17caad714a7f1203d4cc34fbf
NAIBAFFF <fab.andreasson@gmail.com>
2021-05-20 16:33:37 +0200

    Add coordinate calculations and proper experimental graphic generation - Rushing this commit to avoid the merge conflict - Gotta blast

--- src/src/main.rs
+++ src/src/main.rs
@@ -6,7 +6,7 @@
 // std
 use std::io;
 use std::path;
-
+/*
 // Local imports from lib ´backend´ folder
 #[path = "lib/card.rs"]
 mod card;
@@ -14,9 +14,9 @@ mod card;
 mod player;
 #[path = "lib/traits.rs"]
 mod traits;
-
 // Local import structs
 use card::Card;
+*/
 
 // ggez
 use ggez::event;
@@ -29,39 +29,39 @@ use ggez::{Context, GameResult};
 ///
 /// ## Cell size
 /// Size of a cell of which the screen depends on. It is a tuple conataining the float type.
-const CELL_SIZE: (f32, f32) = (160.0, 90.0);
+const CELL_SIZE: (f32, f32) = (135.0, 96.0);
 /// Here we define the size of the playingfield and the screen. The screen size depends
 /// on 2 variables; size of cells and size of cards. This is to make it scale properly.
 ///
 /// ## Card size
 /// Size of a card of which the screen depends on. It is a tuple conataining the float type.
-const CARD_SIZE: (f32, f32) = (160.0, 90.0);
+const CARD_SIZE: (f32, f32) = (135.0, 96.0);
 /// Here we define the size of the playingfield and the screen. The screen size depends
 /// on 2 variables; size of cells and size of cards. This is to make it scale properly.
 ///
 /// ## Amount of Cells
 /// How many cells that the screen will contain.
-const AMOUNT_OF_CELLS: i32 = 12;
+const CELL_AMOUNT: (i32, i32) = (8, 20);
 /// ## Screen size
 /// Decides the size of the screen that will be rendered by ggez. Depends on Cell size and
 /// Amount of Cells.
 const SCREEN_SIZE: (f32, f32) = (
-    CELL_SIZE.0 * AMOUNT_OF_CELLS as f32,
-    CELL_SIZE.1 * AMOUNT_OF_CELLS as f32,
+    CELL_SIZE.0 * CELL_AMOUNT.0 as f32,
+    CELL_SIZE.1 * CELL_AMOUNT.1 as f32,
 );
 
 /// ## Board
 /// Layout of the board that the entire game plays by. This holds the logic for where cards are
 /// stored and it is necessary for generation of graphics.
 struct Board {
-    field: [[Option<Card>; AMOUNT_OF_CELLS as usize]; AMOUNT_OF_CELLS as usize],
-    cards: Vec<Card>,
+    field: [[Option<i8 /*Card*/>; CELL_AMOUNT.0 as usize]; CELL_AMOUNT.1 as usize],
+    //cards: Vec<Card>,
 }
 
 impl Board {
     pub fn new() -> Board {
         Board {
-            field: [[None; AMOUNT_OF_CELLS as usize]; AMOUNT_OF_CELLS as usize],
+            field: [[None; CELL_AMOUNT.0 as usize]; CELL_AMOUNT.1 as usize],
         }
     }
 }
@@ -70,9 +70,9 @@ impl Board {
 struct Game {
     /// Image of the cards and relevant knowledge for
     /// how it should handle inputs
-    sprites: Vec<(Card, graphics::Image)>,
+    /*sprites: Vec<(Card, graphics::Image)>,
     current_player: player::Player,
-    focused_player: player::Player,
+    focused_player: player::Player,*/
     /// Implementation of backend board
     // TODO: Implement it :P
     board: Board,
@@ -84,30 +84,31 @@ impl Game {
         let state = Game {
             board: Board::new(),
         };
-        let player1 = Game::new_player();
+        /*let player1 = Game::new_player();
         Game {
             sprites: Game::load_images(),
             current_player: player1,
             focused_player: player1,
             board: Board::new(),
-        }
+        };*/
         Ok(state)
-    }
-    fn new_player() -> player::Player {
-        // TODO: Generate new player
-    }
-    fn load_sprites() -> Vec<(Card, graphics::Image)> {
-        // TODO: Implement graphics and proper loading function7
-        let mut sprites = Vec::new();
-        sprites.push((Card::new(), "/ccg-test-1.png".to_string()));
-    }
+    } /*
+      fn new_player() -> player::Player {
+          // TODO: Generate new player
+      }
+      fn load_sprites() -> Vec<(Card, graphics::Image)> {
+          // TODO: Implement graphics and proper loading function7
+          let mut sprites = Vec::new();
+          sprites.push((Card::new(), "/ccg-test-1.png".to_string()));
+      }*/
+    /* TODO: Implement Target trait for respective functions
     /// play plays a card in the player's hand, optionally supply targets
     ///
     /// card : the
     /// card_targets is either a Card, Player, or ... which inherits the Target trait
     pub fn play(card: Option<Card>, card_targets: Option<&Target>) -> Result<T, E> {
         // pass
-    }
+    }*/
 }
 
 /// Implementeation of the eventloop in the frontend
@@ -122,19 +123,38 @@ impl event::EventHandler for Game {
         graphics::clear(ctx, [0.5, 0.5, 0.5, 1.0].into());
 
         // Draw playing field
-        for i in 0..(AMOUNT_OF_CELLS * AMOUNT_OF_CELLS) {
-            let board = graphics::Mesh::new_rectangle(
-                ctx,
-                graphics::DrawMode::fill(),
-                graphics::Rect::new_i32(
-                    i % AMOUNT_OF_CELLS * CELL_SIZE.0 as i32,
-                    i / AMOUNT_OF_CELLS * CELL_SIZE.1 as i32,
-                    CELL_SIZE.0 as i32,
-                    CELL_SIZE.1 as i32,
-                ),
-                Color::new(80.0 / 255.0, 80.0 / 255.0, 80.0 / 255.0, 1.0),
-            )?;
-            graphics::draw(ctx, &board, (ggez::mint::Point2 { x: 0.0, y: 0.0 },));
+        for x in 0..CELL_AMOUNT.1 as usize {
+            for y in 0..CELL_AMOUNT.0 as usize {
+                let board = graphics::Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect::new_i32(
+                        x as i32 * CELL_SIZE.1 as i32,
+                        y as i32 * CELL_SIZE.0 as i32,
+                        CELL_SIZE.1 as i32,
+                        CELL_SIZE.0 as i32,
+                    ),
+                    // TODO: Better graphics generation
+                    match y {
+                        1 | 6 => match x {
+                            5..=14 => match x % 2 {
+                                1 => Color::new(87.0 / 255.0, 19.0 / 255.0, 39.0 / 255.0, 1.0),
+                                _ => Color::new(87.0 / 255.0, 19.0 / 255.0, 39.0 / 255.0, 0.5),
+                            },
+                            _ => Color::new(80.0 / 255.0, 80.0 / 255.0, 80.0 / 255.0, 1.0),
+                        },
+                        2 | 5 => match x {
+                            5..=14 => match x % 2 {
+                                1 => Color::new(255.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 1.0),
+                                _ => Color::new(255.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 0.5),
+                            },
+                            _ => Color::new(80.0 / 255.0, 80.0 / 255.0, 80.0 / 255.0, 1.0),
+                        },
+                        _ => Color::new(80.0 / 255.0, 80.0 / 255.0, 80.0 / 255.0, 1.0),
+                    },
+                )?;
+                graphics::draw(ctx, &board, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
+            }
         }
         // TODO: Implement graphics and locations of things
         // TODO: Draw more
@@ -143,21 +163,16 @@ impl event::EventHandler for Game {
     }
     fn key_down_event(
         &mut self,
-        ctx: &mut Context,
-        keycode: KeyCode,
+        _ctx: &mut Context,
+        _keycode: KeyCode,
         _keymods: KeyMods,
         _repeat: bool,
     ) {
         //g
     }
-    fn mouse_button_up_event(
-        &mut self,
-        _ctx: &mut Context,
-        _button: MouseButton,
-        _x: f32,
-        _y: f32,
-    ) {
-        //f
+    fn mouse_button_up_event(&mut self, _ctx: &mut Context, _button: MouseButton, x: f32, y: f32) {
+        let out_index = ((x / CELL_SIZE.1) as isize, (y / CELL_SIZE.0) as isize);
+        println!("{:?}", out_index);
     }
     // Implement functions for events of key presses, etc
     // TODO: Input handler
@@ -171,12 +186,12 @@ fn main() -> GameResult {
 
     // Creates and configures ggez context
     // TODO: Configure correctly
-    let context_builder = ggez::ContextBuilder::new("temp", "kevinwe-faband-nhopkins")
+    let context_builder = ggez::ContextBuilder::new("KThCCG", "kevinwe-faband-nhopkins")
         .add_resource_path(resource_directory)
         .window_setup(ggez::conf::WindowSetup::default().title("KThCCG"))
         .window_mode(
             ggez::conf::WindowMode::default()
-                .dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1)
+                .dimensions(SCREEN_SIZE.1, SCREEN_SIZE.0)
                 .resizable(false),
         );
     // As a sidenote, the documentation for this is outdated- took a bit to figure it out
