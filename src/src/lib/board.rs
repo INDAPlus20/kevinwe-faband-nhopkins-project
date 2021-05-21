53696c7df91c56ff0b7c2dd9e4c09c14780f784b
Kevin Wenström <kevinwenstrom@localhost.localdomain>
2021-05-21 11:21:28 +0200

    make board public

--- src/src/lib/board.rs
+++ src/src/lib/board.rs
@@ -6,7 +6,7 @@ use crate::card::Card;
 /// ## Board
 /// Layout of the board that the entire game plays by. This holds the logic for where cards are
 /// stored and it is necessary for generation of graphics.
-struct Board {
+pub struct Board {
     field: [[Option<Card>; CELL_AMOUNT.0 as usize]; CELL_AMOUNT.1 as usize],
     //cards: Vec<Card>,
 }
